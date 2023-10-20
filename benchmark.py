9689472ee022d831e45a4616477d66f91f94a320
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-10-20 16:02:36 -0500

    benchmark

---
+++ benchmark.py
@@ -0,0 +1,141 @@
+import requests
+import os
+import time
+from concurrent.futures import ThreadPoolExecutor
+import aiohttp
+import asyncio
+import uproot
+
+
+def get_ranges(filename: str, tree_name: str, branch_name: str) -> list((int, int)):
+    """
+    Returns the (start, stop) position of the baskets in the file.
+    """
+    with uproot.open(filename) as f:
+        tree = f[tree_name]
+        branch = tree[branch_name]
+        ranges = [
+            (basket_seek, basket_seek + basket_bytes)
+            for basket_seek, basket_bytes in zip(
+                branch.member("fBasketSeek")[: branch._num_normal_baskets],
+                branch.member("fBasketBytes")[: branch._num_normal_baskets],
+            )
+        ]
+        assert len(ranges) == branch.num_baskets
+        return ranges
+
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+files_directory = os.path.join(script_directory, "files")
+
+
+def request_multipart_range(url, ranges, headers=dict()):
+    range_string = ",".join([f"{start}-{stop - 1}" for start, stop in ranges])
+    headers["Range"] = f"bytes={range_string}"
+    response = requests.get(url, headers=headers)
+    assert response.status_code == 206, f"status code: {response.status_code}"
+    return response
+
+
+def request_single_range(url, /, start: int, stop: int, headers=dict()):
+    headers["Range"] = f"bytes={start}-{stop - 1}"
+    response = requests.get(url, headers=headers)
+    assert response.status_code == 206, f"status code: {response.status_code}"
+    return response
+
+
+def request_single_ranges_blocking(url, ranges, headers=dict()):
+    for start, stop in ranges:
+        yield request_single_range(url, start=start, stop=stop, headers=headers)
+
+
+def request_single_ranges_threading(url, ranges, headers=dict(), num_workers=0):
+    if num_workers <= 0:
+        num_workers = len(ranges)
+    if num_workers > len(ranges):
+        num_workers = len(ranges)
+
+    responses = []
+
+    def worker(start, stop):
+        response = request_single_range(url, start=start, stop=stop, headers=headers)
+        responses.append(response)
+
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
+        for start, stop in ranges:
+            executor.submit(worker, start, stop)
+
+    assert len(responses) == len(ranges)
+    return responses
+
+
+async def request_single_range_async(url, /, start, stop, headers=dict()):
+    headers["Range"] = f"bytes={start}-{stop - 1}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as response:
+            assert response.status == 206, f"status code: {response.status}"
+            return await response.read()
+
+
+def request_single_ranges_async(url, ranges, headers):
+    async def request_ranges():
+        tasks = []
+        for start, stop in ranges:
+            task = request_single_range_async(url, start, stop, headers)
+            tasks.append(task)
+        return await asyncio.gather(*tasks)
+
+    loop = asyncio.get_event_loop()
+    responses = loop.run_until_complete(request_ranges())
+    return responses
+
+
+if __name__ == "__main__":
+    filename = "tree_from_macro_1E7_events.root"
+
+    port = 8080
+    base_path = f"http://localhost:{port}/"
+    file_uri = base_path + filename
+
+    tree_name = "Events"
+    branch_name = "position.z"
+    ranges = get_ranges(file_uri, tree_name=tree_name, branch_name=branch_name)
+    print(f"number of baskets for branch {branch_name}: {len(ranges)}")
+
+    # request the whole file
+    time_start = time.time()
+    response = requests.get(file_uri)
+    time_elapsed = time.time() - time_start
+    print("method: whole file. time elapsed:", time_elapsed)
+
+    # multipart
+    time_start = time.time()
+    response = request_multipart_range(file_uri, ranges=ranges)
+    time_elapsed = time.time() - time_start
+    print("method: multipart. time elapsed:", time_elapsed)
+
+    # worst
+    time_start = time.time()
+    # use the generator
+    responses = [
+        response for response in request_single_ranges_blocking(file_uri, ranges=ranges)
+    ]
+    time_elapsed = time.time() - time_start
+    print("method: blocking (worst). time elapsed:", time_elapsed)
+
+    # threading
+    for num_workers in [0, 4, 10, 20, 50]:
+        time_start = time.time()
+        responses = request_single_ranges_threading(
+            file_uri, ranges=ranges, num_workers=num_workers
+        )
+        time_elapsed = time.time() - time_start
+        print(
+            f"method: threading with {num_workers} workers. time elapsed:", time_elapsed
+        )
+
+    # asyncio
+    time_start = time.time()
+    responses = request_single_ranges_async(file_uri, ranges=ranges, headers=dict())
+    time_elapsed = time.time() - time_start
+    print("method: asyncio. time elapsed:", time_elapsed)
